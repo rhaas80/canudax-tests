d0ab429f6fa111fcda579838aa6ab3df691b459b
Roland Haas <rhaas@illinois.edu>
2022-07-18 15:43:16 -0500

    workflows: first set of fixes

--- bin/build-and-test.sh
+++ bin/build-and-test.sh
@@ -8,23 +8,24 @@ mkdir -p $WORKSPACE
 cd $WORKSPACE
 
 # get a checkout of the gh-pages branch
-cp -rl $SCRIPTSPACE $PAGESSPACE
-( cd $PAGESSPACE && git checkout gh-pages )
+cp -ra $SCRIPTSPACE $PAGESSPACE
+( cd $PAGESSPACE && git checkout -f gh-pages )
 
 # get a checkout of CarpetX
 
-curl -kLO https://raw.githubusercontent.com/gridaphobe/CRL/master/GetComponents
+wget https://raw.githubusercontent.com/gridaphobe/CRL/master/GetComponents
 chmod a+x GetComponents
 ./GetComponents --no-parallel --shallow https://bitbucket.org/eschnett/cactusamrex/raw/master/azure-pipelines/carpetx.th
 
 cd Cactus
 ./simfactory/bin/sim setup-silent --optionlist repos/cactusamrex/azure-pipelines/debian.cfg
 
-time ./simfactory/bin/sim build --thornlist repos/cactusamrex/carpetx.th 2>&1 | tee build.log
+NCPUS=$(nproc)
+time ./simfactory/bin/sim build -j$NCPUS --thornlist repos/cactusamrex/azure-pipelines/carpetx.th 2>&1 | tee build.log
 
-time ./simfactory/bin/sim create-run TestJob01_temp_1 --cores 1 --num-threads 2 --testsuite --testsuite-tests=CarpetX
+time ./simfactory/bin/sim create-run TestJob01_temp_1 --cores 1 --num-threads 2 --testsuite --select-tests=CarpetX
 
-time ./simfactory/bin/sim create-run TestJob01_temp_2 --cores 2 --num-threads 1 --testsuite --testsuite-tests=CarpetX
+time ./simfactory/bin/sim create-run TestJob01_temp_2 --cores 2 --num-threads 1 --testsuite --select-tests=CarpetX
 
 # parse results, generate plots
 

dd2848c973d4038313a479709992d941c8fabf9f
Roland Haas <rhaas@illinois.edu>
2022-07-19 21:33:51 -0500

    mail: produce output similar to logpage

--- bin/mail.py
+++ bin/mail.py
@@ -1,67 +1,67 @@
 '''
 This file sends emails if there are failing tests or if a previously failing test passes now.
 '''
-import requests
-import os
+import os, sys
 from store import get_version
-from parser import test_comp
-
-# TODO: redo using pygit2
-
-runs_list=requests.get(f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/actions/runs").json()
-commit_list=requests.get(f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/commits")
-response=commit_list.json()
-
-current=response[0]["sha"]
-previous=runs_list["workflow_runs"][1]["head_commit"]['id']
-
-compare=requests.get(f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/compare/{previous}...{current}")
-commits=compare.json()["commits"]
-
-
-workflows=requests.get(f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/actions/runs")
-run_id=workflows.json()['workflow_runs'][0]["id"]
-
-# jobs_list=requests.get(f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/actions/runs/{run_id}/jobs")
-# jobs=jobs_list.json()["jobs"]
-# build_job=jobs[0]["steps"][3]
-build_no=get_version()
-curr=f"./records/version_{build_no}/build__2_1_{build_no}.log"
-last=f"./records/version_{build_no-1}/build__2_1_{build_no}.log"
-test_comparison=test_comp(curr,last)
-subject=""
-if len(test_comparison["Failed Tests"])!=0:
-    subject="Some Tests Failed"
-elif len(test_comparison["Newly Passing Tests"])!=0:
-    subject="Some Previously Failing Tests Are Now Passing"
-    
-messages=""
-for commit in commits:
-    messages+=commit["commit"]["message"]+"\n"
-
-repo_name = os.environ['GITHUB_REPOSITORY'].split('/')[-1]
-content=f'''Test
-Build URL: https://{os.environ['GITHUB_REPOSITORY_OWNER']}.github.io/{repo_name}/index_{build_no}
-Project: EinsteinToolkit
-Date of build: {commits[0]["commit"]["committer"]["date"]}
-Changes:
-
-{messages}
-'''
+import logpage
 
 # Import smtplib for the actual sending function
 import smtplib
- 
+
 # Import the email modules we'll need
-from email.message import EmailMessage
- 
-# Create a text/plain message
-msg = EmailMessage()
-msg.set_content(content)
- 
-msg['Subject'] = subject
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+logpage.REPO = sys.argv[1]
+
+# Create the body of the message (a plain-text and an HTML version).
+text = "HTML only email, please see https://einsteintoolkit.github.io/carpetx-tests for output"
+
+curr_ver = get_version()
+summary=f"./records/version_{curr_ver}/build__2_1_{curr_ver}.log"
+baseurl = "https://github.com/einsteintoolkit/carpetx-tests"
+
+data = logpage.create_summary(summary)
+status = "All Tests Passed"
+if data["Number of tests passed"]==0:
+    status="All Tests Passed"
+elif data["Number failed"]!=0:
+    status="Some Tests Failed"
+# Add a table row for each data field
+contents = "\n".join([f"<tr><th>{key}</th><td>{data[key]}</td><tr>" for key in data.keys()])
+
+html = f'''<!doctype html>
+    <html lang="en">
+		<head></head>
+        <body>
+            <h3 style="text-align:center"><a href="{baseurl}/tree/gh-pages/records/version_{curr_ver}">Build #{curr_ver}</a></h3>
+            <table class="table table-bordered " >
+            <caption>Summary</caption>
+            {contents}
+            </table>
+            <table>
+            <caption>Commits in Last Push</caption>
+            {logpage.gen_commits()}
+            </table>
+            {logpage.gen_diffs(summary)}
+		</body>
+	</html>
+'''
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = f"CarpetX test report: {status}"
 msg['From'] = "jenkins@build-test.barrywardell.net"
-msg['To'] = "carpetx-developers@einsteintoolkit.org"
+msg['To'] = "test@einsteintoolkit.org"
+
+# Record the MIME types of both parts - text/plain and text/html.
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+# Attach parts into message container.
+# According to RFC 2046, the last part of a multipart message, in this case
+# the HTML message, is best and preferred.
+msg.attach(part1)
+msg.attach(part2)
  
 # Send the message via our own SMTP server.
 s = smtplib.SMTP('mail.einsteintoolkit.org')

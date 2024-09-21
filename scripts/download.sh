1d4c5fedc105ac76329ee58b69397cc70139ebf3
Cheng-Hsin Cheng <chcheng3@illinois.edu>
2024-09-21 17:16:09 -0500

    Remove step that links arrangements to repo

--- scripts/download.sh
+++ scripts/download.sh
@@ -13,11 +13,3 @@ chmod a+x GetComponents
 ./GetComponents --no-parallel --shallow https://bitbucket.org/canuda/canudax_lean/raw/master/CanudaX.th
 
 cd Cactus
-
-# Create a link to the CanudaX_Lean repository
-ln -s "$CANUDAXSPACE" repos
-# Create links for the CanudaX_Lean
-mkdir -p arrangements/CanudaX_Lean
-pushd arrangements/CanudaX_Lean
-ln -s ../../repos/canudax_lean
-popd
